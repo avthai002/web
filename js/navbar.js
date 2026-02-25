39365749f18edb1a53115be787023d3983185ea9
avthai002 <avthai_002@protonmail.com>
2026-02-26 02:05:42 +0700

    2nd edit

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,11 @@
 const navbarHTML = `
-    <div class="container-fluid d-none d-lg-block" style="background-color: #5D3FD3;">
+    <div class="container-fluid d-none d-lg-block" style="background-color: #E277CD;">
         <div class="row py-2 px-lg-5">
             <div class="col-lg-6 text-left mb-2 mb-lg-0">
             </div>
             <div class="col-lg-6 text-right">
                 <div class="d-inline-flex align-items-center">
-                    <a class="text-white px-2" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="text-white px-2" href="https://wa.me/447946031256"><i class="fab fa-whatsapp"></i></a>
-                    <a class="text-white px-2" href=""><i class="fab fa-instagram"></i></a>
+                    <a class="text-white px-2" href="https://wa.me/447946031256"><i class="fab fa-whatsapp fa-2x"></i></a>
                 </div>
             </div>
         </div>
@@ -15,7 +13,7 @@ const navbarHTML = `
     <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 px-lg-5">
             <a href="index.html" class="navbar-brand ml-lg-3">
-                <h1 class="m-0 text-primary"><span class="text-dark">A&V Thai</span> Massage</h1>
+                <h1 class="m-0 text-primary"><span class="text-primary">A&V Thai</span> Massage</h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
